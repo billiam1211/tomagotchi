d459eb6f3c84092e2e0046ba203f42c1c9c0da21
Bill Bakopoulos <billbakopoulos@Bills-MacBook-Air.local>
2019-04-01 21:25:51 -0500

    Created aging function

--- js/app.js
+++ js/app.js
@@ -1,23 +1,86 @@
 console.log('JS is live');
 
 class Tomagotchi {
-	constructor(name, age, hunger, boredom, sleepiness) {
-		this.name = name
-		this.age = age
-		this.hunger = hunger
-		this.boredom = boredom
-		this.sleepiness = sleepiness
+	constructor(name) {
+		this.name = ''
+		this.age = 0
+		this.hunger = 0
+		this.boredom = 0
+		this.sleepiness = 0
+	}
+}
+
+const pet = new Tomagotchi('', 0, 0, 0, 0)
+
+const game = {
+	lightsOn: true,
+	startGame () {
+		console.log('Start Game initiated');
+		const petName = $('#petName').val()
+		pet.name = petName
+		setInterval(function(){ console.log("3 seconds have passed"); }, 3000);
+		setInterval(function(){ (pet.age += 1); }, 60000);
+	},
+	feed () {
+
+	},
+	lightSwtich () {
+
+	},
+	play () {
+
+	},
+	death () {
+
 	}
 }
 
 $('#feed').on('click', () => {
 	console.log('Feed button was clicked');
+	feed();
 })
 
 $('#lights').on('click', () => {
 	console.log('Lights button was clicked');
+	lightSwtich()
 })
 
 $('#play').on('click', () => {
 	console.log('Play button was clicked');
+	play()
 })
+
+$('form').on('submit', (e) => {
+	e.preventDefault();
+	console.log('form submitted');
+	const inputValue = $('#petName').val()
+	console.log(inputValue);
+	game.startGame();	
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
