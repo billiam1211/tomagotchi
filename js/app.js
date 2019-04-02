b0f7f1c1a09af4dc426f59120e90e9411acfb2b0
Bill Bakopoulos <billbakopoulos@Bills-MacBook-Air.local>
2019-04-02 12:32:11 -0500

    Game Over logic written

--- js/app.js
+++ js/app.js
@@ -22,25 +22,28 @@ const game = {
         pet.name = petName
         console.log(pet.name + ' has been born!');
         setInterval(() => {
-            game.time += 1;
-            $('#time').text(game.time + ' min')
-            if (game.time % 60 === 0) {
+            this.time += 1;
+            $('#time').text(this.time + ' min')
+            if (this.time % 60 === 0) {
                 (pet.age += 1);
                 $('#age').text('Age: ' + pet.age)
             };
-            if (game.time % 10 === 0) {
+            if (this.time % 10 === 0) {
                 pet.hunger += 1
                 $('#hunger').text('Hunger: ' + pet.hunger)
+
+            }
+            if (this.time % 17 === 0) {
                 pet.boredom += 1
                 $('#boredom').text('Boredom: ' + pet.boredom)
             }
-            if (game.time % 30 === 0) {
-                if (game.lightsOn === true) {
+            if (this.time % 30 === 0) {
+                if (this.lightsOn === true) {
                     pet.sleepiness += 1
                     $('#sleepiness').text('Sleepiness: ' + pet.sleepiness)
 
                 } else {
-                    if (game.lightsOn === false) {
+                    if (this.lightsOn === false) {
                         pet.sleepiness -= 1
                         $('#sleepiness').text('Sleepiness: ' + pet.sleepiness)
 
@@ -48,32 +51,35 @@ const game = {
                 }
             };
 
-        }, 250)
+
+
+        }, 150)        
     },
     lightSwtich() {
-        game.lightsOn = !game.lightsOn
+        this.lightsOn = !this.lightsOn
+        console.log(game.lightsOn);
     },
-
     feed() {
-        pet.hunger -= 1
+        if (this.lightsOn = true) {
+            pet.hunger -= 1
+        } else {
+            console.log('Cannot feed ' + pet.name + ' while sleeping');
+        }
     },
-
     play() {
         pet.boredom -= 1
         console.log("You played peek'a'boo with " + pet.name + ".");
     },
-
     death() {
-
+        console.log(pet.name + ' has died...');
     }
 }
-
 $('#feed').on('click', () => {
     console.log('Feed button was clicked');
     game.feed();
 })
 
-$('#lights').on('click', () => {
+$('#lightswitch').on('click', () => {
     console.log('Lights button was clicked');
     game.lightSwtich()
 })
@@ -90,3 +96,30 @@ $('form').on('submit', (e) => {
     console.log(inputValue);
     game.startGame();
 })
+
+
+
+if (pet.hunger = 10 && pet.boredom = 10){
+	this.death()
+} else {
+	if (pet.hunger = 10 && pet.sleepiness = 10) {
+		this.death()
+	} else{
+		if (pet.sleepiness = 10 && pet.boredom = 10){
+			this.death()
+		}
+	}
+}
+
+
+if (pet.hunger = 10 && pet.boredom = 10) {
+		    this.death()
+		} else {
+		    if (pet.hunger = 10 && pet.sleepiness = 10) {
+		        this.death()
+		    } else {
+		        if (pet.sleepiness = 10 && pet.hunger = 10) {
+		            this.death()
+		        }
+		    }
+		}
